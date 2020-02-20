4cd3d02d67a2fab6306858a246e3207e92ad68de
navjotbrar <navb2009@hotmail.com>
2020-02-20 12:56:18 -0700

    Isolated Images and Title

--- WebScraper/WebScraper.py
+++ WebScraper/WebScraper.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://globalnews.ca/news/6571964/democratic-debate-election-nevada/'
+url = 'https://www.nytimes.com/2020/02/20/us/roger-stone-40-months-sentencing-verdict.html'
 response = requests.get(url, timeout=10);
 content = BeautifulSoup(response.content, "html.parser")
+title = content.find('title')
+image = content.find('img').attrs['src']
 
-print(content)
+print(image)
