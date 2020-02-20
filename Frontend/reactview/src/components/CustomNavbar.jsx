d7cb46b94c259665fd0d53c26e4cae4fd6f65dc4
Kelvin Tran <kelvintran4711@gmail.com>
2020-02-20 13:35:32 -0700

    fix navigation bar

--- Frontend/reactview/src/components/CustomNavbar.jsx
+++ Frontend/reactview/src/components/CustomNavbar.jsx
@@ -1,28 +1,19 @@
-import * as React from 'react';
-import {Card, Navbar, Button, Nav, Jumbotron,Form, FormControl} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
-import styled from 'styled-components';
+import React, { Component } from 'react'
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Form, Button, FormControl,} from "react-bootstrap";
 
 
-class CustomNavbar extends React.Component{
-
-    render(){
-
-        return(
-            <>
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">Up2Date</Navbar.Brand>
-                    <Nav className="mr-auto">
-                    <Nav.Link href="#pricing">example1</Nav.Link>
-                    </Nav>
-                    <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-info">Search</Button>
-                    </Form>
-                </Navbar>
-             </>
-        );
-    }
+export default class CustomNavbar extends Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="./">Up2Date</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav></Nav>
+        </Nav>
+          <Button class="p-2" href="./Signup">Sign Up</Button>
+      </Navbar>
+    )
+  }
 }
-
-export default CustomNavbar;
