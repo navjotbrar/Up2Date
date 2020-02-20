bfd15a881bff2e05ec1a368a55d561ea98ed4f50
shamin <shamin99@outlook.com>
2020-02-20 12:59:28 -0700

    added react-router-dom

--- Frontend/reactview/src/App.js
+++ Frontend/reactview/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import CustomNavbar from './components/navbar'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <p>
+        <CustomNavbar />
+      </p>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
--- Frontend/reactview/src/index.js
+++ Frontend/reactview/src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table';
-// import 'react-bootstrap-table-next';
+import { Container } from "reactstrap";
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+            <App />
+    </BrowserRouter>, document.getElementById('root')
+            
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
