9d13474d3fb3bc9b9d5edd02511f1063f13ca38d
navjotbrar <navb2009@hotmail.com>
2020-02-26 02:31:08 -0700

    Flask Endpoints Created, Returns JSON containing Image URL, Title, and Description. Issues: Cannot have a # or ? within URL as the route will cut off URL at those points. This issue is also present within the Requests Python library.

--- WebScraper/WebScraper.py
+++ WebScraper/WebScraper.py
@@ -1,10 +1,40 @@
 from bs4 import BeautifulSoup
+from PIL import Image
+from flask import Flask, jsonify
 import requests
 
-url = 'https://www.nytimes.com/2020/02/20/us/roger-stone-40-months-sentencing-verdict.html'
-response = requests.get(url, timeout=10);
-content = BeautifulSoup(response.content, "html.parser")
-title = content.find('title')
-image = content.find('img').attrs['src']
+app = Flask(__name__)
 
-print(image)
+@app.route('/')
+def emptyReturn():
+	return jsonify({"Title": "none"}, {"Image": "none"}, {"Description": "none"})
+
+@app.route('/urlInfo/<path:link>', methods=['GET'])
+def urlAnalysis(link):
+	url = link
+	response = requests.get(url, timeout=10);
+	content = BeautifulSoup(response.content, "html.parser")
+	title = content.find('title').text
+	for imageURL in content.findAll('img'):
+	    if '.jpg' not in imageURL.attrs['src']:
+	        continue
+	    imageGet = requests.get(imageURL.attrs['src'], stream=True)
+	    imageGet.raw.decode_content = True
+	    img = Image.open(imageGet.raw)
+	    width, height = img.size
+	    if width > 250 or height > 250:
+	        break
+	for description in content.findAll('p'): 
+	    if len(description.text) > 80:
+	        break
+	return jsonify({"Title": title}, {"Image": imageURL.attrs['src']}, {"Description": description.text})
+
+def printTest():
+	print(title.text)
+	print('============================================')
+	print(imageURL.attrs['src'])
+	print('============================================')
+	print(description.text)
+
+if __name__ == '__main__':
+	app.run(debug=True)
