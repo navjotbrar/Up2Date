ab074b68c43472d9e756d480e5f1a6ca4d38a032
avneetgill <avneet2.gill@gmail.com>
2020-02-27 14:46:08 -0700

    image repo and controller, started user services

---
+++ BackendMain/uptodate/src/main/java/com/architecture/uptodate/Repository/ImageRepository.java
@@ -0,0 +1,4 @@
+package com.architecture.uptodate.Repository;
+
+public interface ImageRepository {
+}
---
+++ BackendMain/uptodate/src/main/java/com/architecture/uptodate/Service/UserService.java
@@ -0,0 +1,4 @@
+package com.architecture.uptodate.Service;
+
+public interface UserService {
+}
