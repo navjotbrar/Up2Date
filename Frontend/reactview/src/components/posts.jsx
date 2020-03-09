cd724f0ed0603006308bd515521f1f6808ca9d6b
Kelvin Tran <kelvintran4711@gmail.com>
2020-03-09 16:56:08 -0600

    post component

--- Frontend/reactview/src/components/posts.jsx
+++ Frontend/reactview/src/components/posts.jsx
@@ -1,32 +1,47 @@
-import * as React from 'react';
-import {Table} from "reactstrap"
+import React, { Component, Fragment } from "react";
+import Expand from "react-expand-animated";
 import globe from "./img/world.png";
-export default class Posts extends React.Component{
-    constructor(props){
-        super(props)
+import Login from "./login.jsx"
+import {Table} from "reactstrap"
 
-        this.state ={
+class App extends Component {
+  state = { open: false };
 
-        }
-    }
+  toggle = () => {
+    this.setState(prevState => ({ open: !prevState.open }));
+  };
 
+  render() {
+    const styles = {
+      open: { background: "#ecf0f1" }
+    };
+    const transitions = ["height", "opacity", "background"];
 
-    render(){
-        return(
-            <div> 
-                <Table>
-                    <tr>
-                        <td>
-                            <img src = {globe} width ="100" height="100"/>
-                        </td>
-                        <td>
-                            <p>SAMPLE NEWS TITLE</p>
-                            <p> To Kill a Mockingbird is a novel by Harper Lee published in 1960. Instantly successful, widely read in high schools and middle schools in the United States, it has become a classic of modern American literature, winning the Pulitzer Prize. </p>
-                        </td>
-                    </tr>
-                </Table>
-            </div>
-        )
-    }
+    return (
+      <div onClick={this.toggle}>
+                        <div> 
+                    <Table>
+                        <tr>
+                            <td>
+                                <img src = {globe} width= "100" height = "100"/>
+                            </td>
+                            <td>
+                                <p>Drowing at the olypics</p>
+                                <p> Nope, no one has ever drowned in a swimming race at Olympics, although it was about to happen at 1948 Olympics when Greta Anderson(Denmark) nearly drowned when she fainted midway through a race but was rescued by two other swimmers from Hungary and U.S.. And she went on to win gold medal in 100m freestyle and silver in</p>
+                            </td>
+                        </tr>
+                    </Table>
+                </div>
+          <Expand
+            open={this.state.open}
+            duration={1000}
+            styles={styles}
+          >
+            <img src={globe}/>
+          </Expand>
+      </div>
+    );
+  }
 }
 
+export default App;
