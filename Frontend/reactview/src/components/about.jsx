daead9bd4719217ee4a9e1d0b5bb436aee6db8da
connorbritton <connorbritton2015@gmail.com>
2020-03-12 13:16:11 -0600

    changes to about page

--- Frontend/reactview/src/components/about.jsx
+++ Frontend/reactview/src/components/about.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Form, Button, DropdownButton, Dropdown} from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import { withRouter } from "react-router-dom";
@@ -8,7 +8,7 @@ import {connect} from 'react-redux';
 
 const FormDiv = styled.div`     
     margin: auto;
-    width: 600px;
+    width: 800px;
     background-color: rgb(230, 230, 230);
     display:flex;
     border-radius: .2rem;
@@ -24,15 +24,22 @@ class About extends React.Component{
             <div style = {{paddingTop: '10px', marginTop: '40px'}}>
                 <FormDiv>
                     <Form onSubmit = {this.handleSubmit}>
-                    <p style = {{color: '#23272b', fontWeight: 'bold', fontSize: 20, textAlign: 'left', text: '10px', paddingLeft: '10px'}}>
-                        About Us
+                    <p style = {{color: '#22250b', fontWeight: 'bold', fontSize: 28, textAlign: 'left', text: '10px', paddingLeft: '10px', paddingBottom: '60px'}}>
+                        Our mission is to reinvent how people share, discover, and interact.
                     </p>
                     
                     <Form.Group controlId="formGroupBody">
                         <Form.Label>
                             <p style = {{color: '#23272b', fontWeight: 'normal', fontSize: 16, textAlign: 'left', paddingLeft: '10px'}}>
-                        up2date is developed and produced by an independent group of creative individuals.
-                    </p></Form.Label>
+                                No matter where you are in the world we want to connect your ideas with other people. 
+                                Up2date is designed to facilitate and grow communities without the restrictions 
+                                found on other platforms. 
+                                <br />
+                                <br />                                
+                                Founded in January 2020 and based in Calgary, Alberta, 
+                                up2date is a social media platform 
+                                developed and produced by an independent group of creative individuals.
+                            </p></Form.Label>
                     </Form.Group>
                     </Form>
                 </FormDiv>
