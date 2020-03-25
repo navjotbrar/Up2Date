2c6e844188ddd74aec555de50791c0c2b0ae8f94
Muhammad Qasim <qasimm1998@Hotmail.com>
2020-03-25 17:48:59 -0600

    Spring main app too comment microservice communication and error handling in case comment microservice is down

--- BackendMain/uptodate/src/main/java/com/architecture/uptodate/DTO/CommentDTO.java
+++ BackendMain/uptodate/src/main/java/com/architecture/uptodate/DTO/CommentDTO.java
@@ -1,5 +1,7 @@
 package com.architecture.uptodate.DTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,26 +10,26 @@ import java.util.Date;
 @Getter @Setter
 public class CommentDTO {
 
+    @JsonProperty("commentId")
     private int commentId;
 
+    @JsonProperty("parentCommentId")
     private int parentCommentId;
 
+    @JsonProperty("content")
     private String content;
 
+    @JsonProperty("postId")
     private int postId;
 
+    @JsonProperty("createdDate")
     private Date createdDate;
 
+    @JsonProperty("lastModifiedByDate")
     private Date lastModifiedByDate;
 
-    public CommentDTO(int commentId, String content,int postId){
-        this.commentId=commentId;
-        this.content=content;
-        this.postId=postId;
+    public CommentDTO(){
 
-
-        this.createdDate = new Date();
-        this.lastModifiedByDate = new Date();
     }
 
     public CommentDTO(int commentId,int parentCommentId,String content,int postId){
@@ -39,15 +41,30 @@ public class CommentDTO {
         this.lastModifiedByDate = new Date();
     }
 
-    public CommentDTO(int commentId, int parentCommentId ,String content,int postId, Date createdDate, Date lastModifiedByDate){
+    public CommentDTO(int parentCommentId,String content,int postId){
+        this.content=content;
+        this.postId=postId;
+        this.parentCommentId=parentCommentId;
+        this.createdDate = new Date();
+        this.lastModifiedByDate = new Date();
+    }
+
+    public CommentDTO(int commentId, int parentCommentId,String content,int postId,Date createdDate, Date lastModifiedByDate){
         this.commentId=commentId;
         this.content=content;
         this.postId=postId;
-
-        this.createdDate = new Date();
-        this.lastModifiedByDate = new Date();
-
+        this.parentCommentId=parentCommentId;
+        this.createdDate = createdDate;
+        this.lastModifiedByDate = lastModifiedByDate;
     }
+    public CommentDTO(int commentId,String content,int postId,Date createdDate, Date lastModifiedByDate){
+        this.commentId=commentId;
+        this.content=content;
+        this.postId=postId;
+        this.createdDate = createdDate;
+        this.lastModifiedByDate = lastModifiedByDate;
+    }
+
 
 
 
