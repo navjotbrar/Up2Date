3db1a6241ebbbf6ebd83ccb15e07706928a5748a
connorbritton <connorbritton2015@gmail.com>
2020-03-30 10:44:03 -0600

    no message

--- Frontend/reactview/src/components/main.jsx
+++ Frontend/reactview/src/components/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './login';
 import LandingPage from './landingpage';
 import NewPostView from './newpost'
+import About from './about'
 import { Router, Switch, Route } from 'react-router-dom';
 import Signup from './signup';
 
@@ -11,7 +12,7 @@ const Main = () => (
 
         <Route exact path = "/login" component = {Login} />
         <Route exact path = "/signup" component = {Signup} />
-
+        <Route exact path = "/about" component = {About} />
         <Route exact path = "/newpost" component = {NewPostView} /> 
 
     </Switch>
