e190f5c6bf073872dd54a2821e779420e1e290e1
Muhammad Qasim <qasimm1998@Hotmail.com>
2020-04-02 12:03:18 -0600

    Deleted old test files

--- BackendMain/uptodate/src/main/resources/db/migration/V15__AddChildComments.sql
+++
@@ -1,7 +0,0 @@
-# INSERT INTO up2date.`comments`
-# (content, parentCommentId,postId, createdDate, lastModifiedByDate,author)
-# VALUES('Child Comment 1', 1 ,1, '2020-01-20', '2020-02-20','qasim');
-#
-# INSERT INTO up2date.`comments`
-# (content, parentCommentId,postId, createdDate, lastModifiedByDate,author)
-# VALUES('Child Comment 2', 1 ,1, '2020-01-20', '2020-02-20','qasim');
