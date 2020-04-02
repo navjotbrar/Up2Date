8debe6888c4e63ad1584e630b432e04721abe60c
Muhammad Qasim <qasimm1998@Hotmail.com>
2020-04-02 12:21:42 -0600

    Better test post

--- BackendMain/uptodate/src/main/resources/db/migration/V13__PostDummyData.sql
+++ BackendMain/uptodate/src/main/resources/db/migration/V13__PostDummyData.sql
@@ -1,3 +1,3 @@
 INSERT INTO up2date.`posts`
-(title, body, createdate, author, link)
-VALUES('ExamplePost', 'I like to eat chocolate','2020-01-20','qasim', 'https://www.confectionerynews.com/Article/2020/03/25/Virtual-global-chocolate-festival-StayHomeWithChocolate-launched-to-help-small-businesses');
+(title, body, createdate, author, link,imageurl,articletitle)
+VALUES('Deaths toll over 10000', 'Coronavirus over 10,000 deaths!','2020-01-20','qasim', 'https://www.bbc.com/news/world-europe-52131756','https://ichef.bbci.co.uk/news/320/cpsprodpb/D72A/production/_111528055_mediaitem111528054.jpg','The country, the second-worst hit in terms of deaths, has also lost nearly 900,000 jobs. The US on Friday said it saw  a record 6.6 million new unemployment benefit claims');
