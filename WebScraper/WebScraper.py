17fed13cd9b93edb562f511312d4a2bcbad81f8c
navjotbrar <navb2009@hotmail.com>
2020-04-02 20:53:32 -0600

    Fixed Errors, Took off Debug Mode

--- WebScraper/WebScraper.py
+++ WebScraper/WebScraper.py
@@ -12,11 +12,9 @@ app = Flask(__name__)
 def emptyReturn():
 	return jsonify({"Title": 0, "Image": 0, "Excerpt": 0})
 
-#TODO: Handle '?' and '#' within the path variable
 #This route is called for API users to get information about the Title, main Image, Excerpt of a News website
 @app.route('/urlInfo/<path:link>', methods=['GET'])
 def urlAnalysis(link):
-	#Temporary solution to query parameter issue
 	stockImage = 'https://cdn.onlinewebfonts.com/svg/img_148071.png'
 	if len(request.query_string) > 0:
 		return jsonify({"Title": "Could not decode website", "Image": stockImage, "Excerpt": "Click on the link to view the article"})
@@ -32,34 +30,32 @@ def urlAnalysis(link):
 		return jsonify({"Title": "Could not decode website", "Image": stockImage, "Excerpt": "Click on the link to view the article"})
 	#Find a paragraph that can be used as a short excerpt from the article by disregarding paragraphs shorter than a certain size
 	for excerpt in content.findAll('p'): 
-		if len(excerpt.text) > 80:
-			break
-	for imageURL in content.findAll('img'):
-		if '.jpg' not in imageURL.attrs['src']:
-			continue
 		try:
+			if len(excerpt.text) > 80:
+				break
+		except:
+			return jsonify({"Title": title, "Image": stockImage, "Excerpt": "Click on the link to view the article"})
+	for imageURL in content.findAll('img'):
+		try:
+			if '.jpg' not in imageURL.attrs['src']:
+				continue
 			imageGet = requests.get(imageURL.attrs['src'], stream=True)
 			imageGet.raw.decode_content = True
 			img = Image.open(imageGet.raw)
+			width, height = img.size
+			if width > 250 or height > 250:
+				break
 		except:
 			return jsonify({"Title": title, "Image": stockImage, "Excerpt": excerpt.text})
-		width, height = img.size
-		if width > 250 or height > 250:
-			break
 	#Return a JSON containing Title, Image, and Excerpt variables with values scraped from the HTML of the website
-	return jsonify({"Title": title, "Image": imageURL.attrs['src'], "Excerpt": excerpt.text})
-
-#TODO: Remove this fuction when application is fully tested
-#Test function that prints out variable information
-def printTest():
-	print(title.text)
-	print('============================================')
-	print(imageURL.attrs['src'])
-	print('============================================')
-	print(excerpt.text)
+	try:
+		if '.jpg' not in imageURL.attrs['src']:
+				return jsonify({"Title": title, "Image": stockImage, "Excerpt": excerpt.text})
+		return jsonify({"Title": title, "Image": imageURL.attrs['src'], "Excerpt": excerpt.text})
+	except:
+		return jsonify({"Title": "Could not decode website", "Image": stockImage, "Excerpt": "Click on the link to view the article"})
 
 #When script is executed, Python assigns the name '__main__' to the script
 if __name__ == '__main__':
-	#TODO: Change debug to 'False' after application is tested 
 	#Runs the App, debug allows us to trace errors
-	app.run(debug=True)
+	app.run(debug=False)
